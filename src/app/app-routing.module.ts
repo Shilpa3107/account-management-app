e5e0d231edd7f46b5868688234fb2c20a0262fd3
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-03 04:40:52 +0530

    imported the three components and created its path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountInfoComponent } from 'src/account-info/account-info.component';
+import { LoginComponent } from 'src/login/login.component';
+import { RegistrationComponent } from 'src/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'account-info', component: AccountInfoComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
