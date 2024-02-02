c6a40ec4c99aa5525072cbfc1f4856bd62e862c9
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-03 05:13:23 +0530

    Imported edit -account component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountInfoComponent } from 'src/account-info/account-info.component';
+import { EditAccountComponent } from 'src/edit-account/edit-account.component';
 import { LoginComponent } from 'src/login/login.component';
 import { RegistrationComponent } from 'src/registration/registration.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'registration', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'account-info', component: AccountInfoComponent },
+  { path: 'edit-account', component: EditAccountComponent},
   { path: '', redirectTo: '/registration', pathMatch: 'full' },
 ];
 
