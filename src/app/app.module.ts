4a62ec570490f6f955af5315fcb58d9af7a00d0e
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-03 04:49:12 +0530

    Imported forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from 'src/login/login.component';
+import { RegistrationComponent } from 'src/registration/registration.component';
+import { AccountInfoComponent } from 'src/account-info/account-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    AccountInfoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
