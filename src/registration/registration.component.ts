d48f370c9ad642c75c33ba8c023b0d6377d087d5
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-03 04:52:00 +0530

    Added the registration mechanism

--- src/registration/registration.component.ts
+++ src/registration/registration.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent {
+  username = '';
+  password = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  register() {
+    const success = this.authService.register(this.username, this.password);
+    if (success) {
+      this.router.navigate(['/account-info']);
+    } else {
+      alert('Registration failed. Please try again.');
+    }
+  }
 }
