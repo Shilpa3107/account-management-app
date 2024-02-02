ee68602dd3ba5467179ef1590f95d26b12fd45b0
Shilpa <shilpa.sinha3107@gmail.com>
2024-02-03 04:51:14 +0530

    Taking username and password as login

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  username = '';
+  password = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    const success = this.authService.login(this.username, this.password);
+    if (success) {
+      this.router.navigate(['/account-info']);
+    } else {
+      alert('Invalid credentials. Please try again.');
+    }
+  }
 }
